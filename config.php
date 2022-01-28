81acc76e9d9dd200164c4e23d68ae5f23871be8a
Wesley Carvalho <crafael.wesley@gmail.com>
2022-01-28 14:23:53 -0300

    Deploy

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/* Credenciais do banco de dados. Supondo que você esteja executando o MySQL
+servidor com configuração padrão (usuário 'root' sem senha) */
+define('DB_SERVER', '162.241.2.253');
+define('DB_USERNAME', 'eagles29_root');
+define('DB_PASSWORD', 'eAgles2709$');
+define('DB_NAME', 'eagles29_login');
+ 
+/* Tentativa de conexão com o banco de dados MySQL */
+try{
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    // Defina o modo de erro PDO para exceção
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Não foi possível conectar." . $e->getMessage());
+}
+?>
